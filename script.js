de44581fe0a3eeb2b579e5ca04b7cfe5944a7bc0
vlad10md <vlad.iovu80@gmail.com>
2023-03-10 22:00:47 +0000

    Added more rounds to be played and a score system

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let playerScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   let computer_choice = Math.floor(Math.random() * choices.length);
@@ -16,17 +20,38 @@ function playRound(playerSelection, computerSelection) {
       (playerSelection == "paper" && computerSelection == "rock") ||
       (playerSelection == "scissors" && computerSelection == "paper")) {
       console.log(`player played ${playerSelection}, computer player ${computerSelection}. player wins`);
+      playerScore++
+      console.log('player score is ' + playerScore + 'computer score is ' + computerScore);
       }
   else if (computerSelection == playerSelection) {
     console.log(`player played ${playerSelection}, computer played ${computerSelection}. it's a tie`);
+    console.log('player score is ' + playerScore + 'computer score is ' + computerScore);
   } else {
     console.log(`player played ${playerSelection}, computer played ${computerSelection}. computer wins`);
+    computerScore ++
+    console.log('player score is ' + playerScore + 'computer score is ' + computerScore);
   }
   
 }
-    
+ 
+let computerSelection;
+let playerSelection;
 
-const computerSelection = getComputerChoice();
-const playerSelection = getPlayerChoice();
+function game() {
+ for (let i = 0; i < 5; i++) {
+  const computerSelection = getComputerChoice();
+  const playerSelection = getPlayerChoice();
+  playRound(playerSelection,computerSelection);
+ }
+ if (playerScore > computerScore){
+    console.log('Player wins');
+  } else if (computerScore > playerScore) {
+    console.log('Computer wins');
+  } else{ 
+    console.log('Game over, it ends in a draw');
+  }
+}
 
-console.log(playRound(playerSelection,computerSelection));
+
+
+game();
